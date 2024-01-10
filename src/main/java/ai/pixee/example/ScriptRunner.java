47ee561e3f8d4d4cc23b19de6dfb6c02f3223ab6
Ryan Dens <ryan.dens@pixee.ai>
2024-01-10 14:42:59 -0800

    :sparkles: Add example vulnerable code

---
+++ src/main/java/ai/pixee/example/ScriptRunner.java
@@ -0,0 +1,30 @@
+package ai.pixee.example;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
+import java.util.HashSet;
+import java.nio.file.Files;
+
+public final class ScriptRunner {
+
+    public void create(Path script) throws IOException {
+        final var filePermissions = new HashSet<PosixFilePermission>();
+        filePermissions.add(PosixFilePermission.OTHERS_WRITE);
+        filePermissions.add(PosixFilePermission.OTHERS_EXECUTE);
+        Files.setPosixFilePermissions(script,filePermissions);
+        Runtime.getRuntime().exec(script.toString());
+    }
+
+    public void updateNewScript(Path startupScript, Path shutdownScript) throws IOException {
+        // why not have anybody be able to execute?
+        Files.setPosixFilePermissions(startupScript, PosixFilePermissions.fromString("rwxrwx---"));
+
+        // same for script
+        Set<PosixFilePermission> permissions = PosixFilePermissions.fromString("rwxrwx---");
+        Files.setPosixFilePermissions(shutdownScript, permissions);
+    }
+
+}
